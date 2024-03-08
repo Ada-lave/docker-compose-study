1ade367b7ffef8bfde1d2db4cda407338e77104a
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-03-08 18:51:01 +0500

    init

---
+++ api/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/test", (req, res) => {
+    res.send("API WORKS")
+})
+
+
+app.listen(3000, ()=>{
+    console.log("API SERVER START")
+})
